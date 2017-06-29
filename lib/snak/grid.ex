06949da1bd84cf634f15a943044c4efeb79baded
Sean Chamberlain <seanlchamberlain@gmail.com>
2017-06-29 14:23:04 +0930

    Make move snake a pipeline

--- lib/snak/grid.ex
+++ lib/snak/grid.ex
@@ -11,38 +11,54 @@ defmodule Snak.Grid do
   def new, do: changeset(%__MODULE__{}, %{snake: @init_snake}) |> generate_apple
 
   def move_snake(game = %Game{direction: direction, grid: grid}) do
-    with {:ok, head}    <- current_head(grid.snake),
-         {:ok, head}    <- move_head(head, direction),
-         {status}       <- perform_collision_checks(head, grid),
-         {:ok, snake}   <- add_head_to_snake(head, grid.snake),
-         {:ok, changes} <- remove_tail_from_snake(status, snake),
-         do: {game, %{grid: changeset(grid, changes, status)}, status}
+    {grid, status} = current_head(grid.snake)
+      |> validate_move(grid, direction)
+      |> perform_move
+
+    {game, %{grid: grid}, status}
   end
 
-  defp current_head(snake), do: {:ok, :queue.get(snake)}
+  defp validate_move(head, grid, direction) do
+    head
+      |> move_head(direction)
+      |> perform_collision_checks(grid)
+  end
 
-  defp move_head(position, :up),    do: {:ok, %{x: position.x, y: position.y - 1}}
-  defp move_head(position, :down),  do: {:ok, %{x: position.x, y: position.y + 1}}
-  defp move_head(position, :left),  do: {:ok, %{x: position.x - 1, y: position.y}}
-  defp move_head(position, :right), do: {:ok, %{x: position.x + 1, y: position.y}}
+  defp perform_move({head, grid, status}) do
+    grid = head
+     |> add_head(grid.snake)
+     |> remove_tail(status)
+     |> (&changeset(grid, &1, status)).()
 
-  defp add_head_to_snake(head, snake), do: {:ok, :queue.in_r(head, snake)}
+     {grid, status}
+  end
+
+  defp current_head(snake), do: :queue.get(snake)
+
+  defp move_head(position, :up),    do: %{x: position.x, y: position.y - 1}
+  defp move_head(position, :down),  do: %{x: position.x, y: position.y + 1}
+  defp move_head(position, :left),  do: %{x: position.x - 1, y: position.y}
+  defp move_head(position, :right), do: %{x: position.x + 1, y: position.y}
+
+  defp add_head(head, snake), do: :queue.in_r(head, snake)
 
   defp perform_collision_checks(head, grid) do
-    cond do
-      head == grid.apple              -> {:ate_apple}
-      head.x == 1 or head.x == 40     -> {:collision}
-      head.y == 1 or head.y == 24     -> {:collision}
-      :queue.member(head, grid.snake) -> {:collision}
-      true                            -> {:ok}
+    status = cond do
+      head == grid.apple              -> :ate_apple
+      head.x == 1 or head.x == 40     -> :collision
+      head.y == 1 or head.y == 24     -> :collision
+      :queue.member(head, grid.snake) -> :collision
+      true                            -> :ok
     end
+
+    {head, grid, status}
   end
 
-  defp remove_tail_from_snake(:ok, snake) do
+  defp remove_tail(snake, :ok) do
     {{_, old_tail}, snake} = :queue.out_r(snake)
-    {:ok, %{snake: snake, old_tail: old_tail}}
+    %{snake: snake, old_tail: old_tail}
   end
-  defp remove_tail_from_snake(_, snake), do: {:ok, %{snake: snake, old_tail: nil}}
+  defp remove_tail(snake, _), do: %{snake: snake, old_tail: nil}
 
   defp changeset(grid, opts),             do: struct(grid, opts)
   defp changeset(grid, opts, :ate_apple), do: changeset(grid, opts) |> generate_apple
