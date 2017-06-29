460eb820b8eb4d647867dbfb3c6ad79f1e28f452
Sean Chamberlain <seanlchamberlain@gmail.com>
2017-06-29 13:38:55 +0930

    Remove unneeded class

--- lib/snak/cli.ex
+++
@@ -1,8 +0,0 @@
-defmodule Snik.CLI do
-  def main(_args) do
-    # GenServer.start_link(Snik.Game, [], name: Game)
-    # GenServer.start_link(Snik.IO, [], name: IO)
-
-    :erlang.hibernate(Kernel, :exit, [:killed])
-  end
-end
